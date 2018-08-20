dc0ba865ec888c2a809464fb89216f8d02db9863
Ramin Banihashemi <ramin@casevacanza.it>
2018-08-20 15:57:15 +0200

    fixed the visualization of the app logs configured on the docker volumes

--- custom/logs.sh
+++ custom/logs.sh
@@ -3,10 +3,21 @@ DESCRIPTION='View the project logs'
 
 if [ "${1}" == 'go' ]; then
     setImageName "php"
+    OS=`uname -s`
     getContainer
     if [ -z $container ]; then
         show "\n<red>✗ Container not found</red>\n"
     else
-        execute "docker exec -it $container bash -c 'source ~/.bashrc && tail -f /var/www/var/log/*'";
+        logpath=$(docker inspect --format '{{ .Mountpoint }}' ${docker_namespace}_log-data)
+        if [ $OS != "Darwin" ]; then
+            execute "tail -f ${logpath}/*"
+        else
+            logFiles=$(docker run -it --rm --privileged --pid=host justincormack/nsenter1 /bin/sh -c "ls -A ${logpath}/")
+            if [ -z "${logFiles}" ]; then
+                show "\n<red>✗ No log files found</red>\n"
+            else
+                execute "docker run -it --rm --privileged --pid=host justincormack/nsenter1 /bin/sh -c 'tail -f ${logpath}/*'"
+            fi
+        fi
     fi
 fi
